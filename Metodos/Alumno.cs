45dd5aea69b9198a21a1495ff145ef5d99e9023a
Bidkar Aragón Cárdenas <bidkar.aragon@uadeo.mx>
2024-04-15 08:24:03 -0700

    Sobrecarga y anulación de métodos

---
+++ Metodos/Alumno.cs
@@ -0,0 +1,39 @@
+﻿namespace Metodos;
+
+public class Alumno
+{
+    public int Id { get; set; }
+    public string Nombres { get; set; }
+    public string Apellidos { get; set; }
+    public DateOnly FechaDeNacimiento { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id:{Id}, Nombre:{NombreCompleto()}, Nacido en: {FechaDeNacimiento}";
+    }
+
+    public string NombreCompleto()
+    {
+        return $"{Nombres} {Apellidos}";
+    }
+
+    public string NombreCompleto(bool primeroApellidos)
+    {
+        if (primeroApellidos)
+            return $"{Apellidos} {Nombres}";
+        else
+            return NombreCompleto();
+    }
+
+    public string NombreCompleto(bool primeroApellidos, bool enMayusculas)
+    {
+        if (primeroApellidos && enMayusculas)
+            return NombreCompleto(true).ToUpper();
+        else if (primeroApellidos && !enMayusculas)
+            return NombreCompleto(true).ToLower();
+        else if (!primeroApellidos && enMayusculas)
+            return NombreCompleto().ToUpper();
+        else
+            return NombreCompleto().ToLower();
+    }
+}
--- Metodos/Program.cs
+++ Metodos/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Metodos;
+
+var alumno1 = new Alumno()
+{
+    Id = 1,
+    Nombres = "Juan Manuel",
+    Apellidos = "Alcantar Rubio"
+};
+
+// imprimir el nombre completo del alumno iniciando con el nombre
+Console.WriteLine(alumno1.NombreCompleto());
+
+// imprimir el nombre completo del alumno iniciando por apellidos
+Console.WriteLine(alumno1.NombreCompleto(true));
+
+// imprimir el nombre completo del alumno iniciando con apellidos y en minusculas
+Console.WriteLine(alumno1.NombreCompleto(primeroApellidos: true, enMayusculas: false));
+
+// imprimir el nombre completo del alumno iniciando con apellidos y en mayusculas
+Console.WriteLine(alumno1.NombreCompleto(true, true));
+
+// imprimir el nombre completo del alumno iniciando con nombre y en mayusculas
+Console.WriteLine(alumno1.NombreCompleto(false, true));
+
+// imprimir el nombre completo del alumno iniciando con nombre y en minusculas
+Console.WriteLine(alumno1.NombreCompleto(false, false));
+
+// imprimir lo que salga del método ToString() del alumno
+Console.WriteLine(alumno1.ToString());
